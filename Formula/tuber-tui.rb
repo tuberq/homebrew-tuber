7edc952ead7bc383fde7ca8e33c61bbd5f60dff4
Dan Milne <d@nmilne.com>
2026-03-26 08:42:52 +1100

    Updates to tuber-tui

--- Formula/tuber-tui.rb
+++ Formula/tuber-tui.rb
@@ -1,28 +1,28 @@
 class TuberTui < Formula
   desc "Terminal UI for tuber work queue"
   homepage "https://github.com/tuberq/tuber-tui"
-  version "0.4.2"
+  version "0.4.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.2/tuber-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "0254629344918bbdd318f4194817a26681271e470f76d27e9973c4734e7870f9"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.3/tuber-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "cdc87d37a76d2b566963f2fe7fc45f1defe164da1a75ea41a7de3d431f636720"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.2/tuber-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "1c84a5490fe3e01e610ae5337c47b78a1f594573a33ef8ec4ea9a023b3bffe92"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.3/tuber-tui-x86_64-apple-darwin.tar.gz"
+      sha256 "feed40fd4c769a184e48f7f418ed1813de0153cdd2b24bb08a63bdc075d2c301"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.2/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "c28d39c2fc39b74a0de91a04a355e8df0d2a5b6a0250961796052cade9b58979"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.3/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "24d1b77d6660b064dbda77a9ae65678d16e91a9853a37cf7ed567f3512e71e02"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.2/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "6004b64aa54c3031f4aedb2a8d3288befe9bace03f6b18b405b800f4a89fba2e"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.4.3/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "cac3c7d2a6077dd5c26fd1f84db85b19a6dab36b6913422ffc2ecd8733d983ac"
     end
   end
 
