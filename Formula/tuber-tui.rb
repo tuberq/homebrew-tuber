82d64724c6a7cc5358b6bd8562980a52690af03a
Dan Milne <d@nmilne.com>
2026-04-06 10:56:17 +1000

    Version bump for tuber-tui

--- Formula/tuber-tui.rb
+++ Formula/tuber-tui.rb
@@ -1,28 +1,28 @@
 class TuberTui < Formula
   desc "Terminal UI for tuber work queue"
-  homepage "https://github.com/tuberq/tuber-rs"
-  version "0.6.0"
+  homepage "https://github.com/tuberq/tuber-tui"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tuberq/tuber-rs/releases/download/v0.6.0/tuber-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "1cb71197fd169a7bee458b3e1a08c9045f14be036d006ffbb965ce34309e801f"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.6.1/tuber-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "d27682a08d6024fd6b23f095d16c374cd33aa98ee214c48f5524410be57cba6a"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-rs/releases/download/v0.6.0/tuber-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "d1d02454e373d5c60dd7577c5c7fcf38ce28b259b6c5561d9487c5c286686139"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.6.1/tuber-tui-x86_64-apple-darwin.tar.gz"
+      sha256 "8314599d56f94fef0327bb2098abb519f94f321d4c5e12f16d2ab07514eca01f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tuberq/tuber-rs/releases/download/v0.6.0/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "c168740aefeb7bba844c850354ec844d011d590cd0ab3fd15c9b6244457b9967"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.6.1/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "82d885569bbdcf2340d77e3eb4554a4a12098763e587fd7bdd2e2d81b80a796d"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-rs/releases/download/v0.6.0/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "90b49ee3b90594590e93ef4c8c828e4e6813cf6cba9982197153be4207edf384"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.6.1/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "8b575c98c447825a6671a68fa52bebf393798ed3b468ba49957305da8599b760"
     end
   end
 
